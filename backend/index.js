cb58412fc9261b4d9febdd74e127ddbd00171758
Lakshya2610 <lakshyagupta26@live.com>
2020-03-21 01:27:13 -0400

    added profile card template and routes for more stuff

--- backend/index.js
+++ backend/index.js
@@ -15,22 +15,38 @@ app.get("/close", (req, res) => {
     res.status(200).redirect("/");
 });
 
+app.get("/fetchProfiles", (req, res) => {
+    // 1. query user profile first for crs codes that are related to this user
+    // 2. Fetch cards with same courses
+    // 3. Filter according to additional req if necessary
+    // 4. Send response with all the valid cards (JSON)
+});
+
+app.post("/newProfileCard", urlEncodedParser, (req, res) => {
+    // 1. Check if a profile card already exists linked to the user
+    //     a. If it does, add this crs code as well
+    // 2. Otherwise create a new profile card in the database *Profiles*
+});
+
 app.post("/new-user", urlEncodedParser, (req, res) => {
     const requestData = {
         name: req.body.name,
         email: req.body.email,
         gender: req.body.gender,
         uni: req.body.uni,
-        major: req.body.major,
+        major: req.body.major, // don't need it
         age: Number(req.body.age)
     };
 
+    // database *Users*
     DatabaseManager.insertProfile(requestData).then((result) => {
         // sendEmail(requestData);
         // reply with success response code
-        
+
     }).catch((err) => {
         // unsuccessful insert, reply back with unsuccess response code
+        console.log(err);
+        res.status(500).send("Insert Failed");
     });
 
     res.status(201).redirect("/");
