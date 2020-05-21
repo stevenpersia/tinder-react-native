b87bfd6fc4a3afdbfee0f223e7c0fa57d16146f9
Lakshya2610 <lakshyagupta26@live.com>
2020-05-20 23:57:56 -0400

    added sign up logic

--- app/containers/SignUp.js
+++ app/containers/SignUp.js
@@ -4,6 +4,8 @@ import styles from '../assets/styles';
 import { DefaultTheme, Provider as PaperProvider, TextInput, RadioButton, Dialog, Button } from 'react-native-paper';
 import DatePicker from 'react-native-datepicker';
 
+import Fetcher from '../assets/data/Fetcher';
+
 const labelStyle = { 
     colors: { 
         text: 'white', 
@@ -34,8 +36,29 @@ class SignUp extends React.Component {
             emailMode: 'outlined',
             passMode: 'outlined',
             date: null,
-            checked: 'first'
+            checked: 'first',
+            name: "",
+            email: "",
+            password: ""
         };
+    }
+
+    async handleSubmit() {
+        // TODO: validate input
+        const requestHandler = new Fetcher();
+        const data = {
+            name: this.state.name,
+            email: this.state.email,
+            password: this.state.password,
+            // uni: req.body.uni,
+            // major: req.body.major,
+            age: this.state.date,
+            // image: req.body.image
+        }
+
+        const signUpStatus = await requestHandler.requestSignUp(data);
+        // check if it works
+        console.log('Sign up status: ' + signUpStatus);
     }
 
     render() {
@@ -75,18 +98,24 @@ class SignUp extends React.Component {
                 </CardStack> */}
                     <TextInput
                     mode={this.state.nameMode}
+                    value={this.state.name}
                     label='Name'
                     placeholder="Enter your full name"
                     onFocus={() => this.setState({ nameMode: 'flat' })}
+                    onBlur={() => { if(this.state.name.length === 0) { this.setState({ nameMode: 'outlined' }); } }}
+                    onChangeText={(text) => this.setState({ name: text })}
                     theme={labelStyle}
                     style={textBoxStyle}
                     />
 
                     <TextInput
                     mode={this.state.emailMode}
+                    value={this.state.email}
                     label='Email'
                     placeholder="email@example.com"
                     onFocus={() => this.setState({ emailMode: 'flat' })}
+                    onBlur={() => { if(this.state.email.length === 0) { this.setState({ emailMode: 'outlined' }); } }}
+                    onChangeText={(text) => this.setState({ email: text })}
                     theme={labelStyle}
                     style={textBoxStyle}
                     />
@@ -94,9 +123,12 @@ class SignUp extends React.Component {
                     <TextInput
                     secureTextEntry={true}
                     mode={this.state.passMode}
+                    value={this.state.password}
                     label='Password'
                     placeholder="Enter your new password"
                     onFocus={() => this.setState({ passMode: 'flat' })}
+                    onBlur={() => { if(this.state.password.length === 0) { this.setState({ passMode: 'outlined' }); } }}
+                    onChangeText={(text) => this.setState({ password: text })}
                     theme={labelStyle}
                     style={textBoxStyle}
                     />
@@ -119,6 +151,10 @@ class SignUp extends React.Component {
                     onDateChange={(date) => {this.setState({date: date})}}
                     androidMode='spinner'
                     />
+
+                    {/* <Button mode='outlined' color='white' onPress={this.handleSubmit.bind(this)}>
+                        Submit
+                    </Button> */}
             </View>
         );
     }
