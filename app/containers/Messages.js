e7eb92210a0239c9746d5f27a8717a1383f864ed
= <parshvashah414@gmail.com>
2020-05-22 19:27:19 -0400

    profile page

--- app/containers/Messages.js
+++ app/containers/Messages.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import styles from '../assets/styles';
-
+//Profile
 import {
   ScrollView,
   Text,
   TouchableOpacity,
   ImageBackground,
   View,
-  FlatList
+  FlatList,
 } from 'react-native';
 import Message from '../components/Message';
 import Icon from '../components/Icon';
@@ -25,7 +25,7 @@ const Messages = () => {
             <Text style={styles.title}>Messages</Text>
             <TouchableOpacity>
               <Text style={styles.icon}>
-                <Icon name="optionsV" />
+                <Icon name='optionsV' />
               </Text>
             </TouchableOpacity>
           </View>
